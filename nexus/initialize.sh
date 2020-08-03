953694119af7961ee4e867cb3d3b44569a742983
Jumpaku <itzimeru@gmail.com>
2020-08-03 22:45:08 +0900

    fix: update reverse proxy version to jumpaku/nginx-letsencrypt-reverse-proxy:release-v1.1.0

--- nexus/initialize.sh
+++ nexus/initialize.sh
@@ -14,6 +14,22 @@ Setup nexus3 in browser:
 1. Change admin's password.
 2. Allow anonymous access.
 3. Create LDAP connection and login as a connected user.
+  * Name: ldap
+  * LDAP server address: ldap://openldap:389
+  * Serch base DN: ou=users,dc=jumpaku,dc=net
+  * Authantication method: Simple Authantication
+  * Username or DN: cn=app,dc=jumpaku,dc=net
+
+  * Configuration template: none
+  * User subtree: checked
+  * Object class: inetOrgPerson
+  * User filter: none
+  * User ID attribute: uid
+  * Real name attribute: uid
+  * Email attribute: mail
+  * Password attribute: userPassword
+  * Map LDAP groups as roles: unchecked
+
 4. Create Role with Privilege as follows and Role to the user.
 
   * nx-component-upload
