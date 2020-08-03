b9c3ec5a972252541069f907fd32cddb597293e6
Jumpaku <itzimeru@gmail.com>
2020-08-03 09:46:59 +0900

    refactor(ldap): change docker image to jumpaku/ldap-docker

--- ldap/ldap-client/data/display.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-ldapsearch -x -h openldap -D "cn=admin,${LDAP_SUFFIX}" -w ${LDAP_ROOT_PW} -b "${LDAP_SUFFIX}" "(objectClass=*)"
-
--- ldap/ldap-client/data/init.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-ldapadd -x -h openldap -D "cn=admin,${LDAP_SUFFIX}" -w ${LDAP_ROOT_PW} -f rootdn.ldif
-
